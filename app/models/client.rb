c7d257ddd23393545484ee64c6132e8c148f3f0e
Robert Sefer <rsefer@gmail.com>
2014-03-25 16:20:04 -0500

    Added validation for paperclip image upload

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,5 @@
 class Client < ActiveRecord::Base
 	has_many :projects
 	has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/clients/:style/missing.png"
+	validates_attachment_content_type :logo, :content_type => %w(image/jpeg image/jpg image/png)
 end
