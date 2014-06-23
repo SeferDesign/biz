330188c3fe99039917af34579280668b0855ebf9
Robert Sefer <rsefer@gmail.com>
2014-06-23 18:41:49 -0500

    Tweaks

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,12 +1,12 @@
 class LinesController < ApplicationController
-	
+
 	def create
     @invoice = Invoice.find(params[:invoice_id])
     @line = @invoice.lines.create(params[:line].permit(:description, :hourly, :hours, :rate, :total))
     @invoice.update(:cost => @invoice.lines.sum('total'))
     redirect_to invoice_path(@invoice)
   end
-  
+
   def destroy
     @invoice = Invoice.find(params[:invoice_id])
     @line = @invoice.lines.find(params[:id])
@@ -14,5 +14,5 @@ class LinesController < ApplicationController
     @invoice.update(:cost => @invoice.lines.sum('total'))
     redirect_to invoice_path(@invoice)
   end
-  
+
 end
