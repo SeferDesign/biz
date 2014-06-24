9bdbd55faecc016d64954bc73e10fd611b1d03db
Robert Sefer <rsefer@gmail.com>
2014-06-23 20:33:46 -0500

    Converted to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140623234822) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "clients", force: true do |t|
     t.string   "name"
     t.string   "contact"
@@ -72,7 +75,7 @@ ActiveRecord::Schema.define(version: 20140623234822) do
     t.datetime "updated_at"
   end
 
-  add_index "lines", ["invoice_id"], name: "index_lines_on_invoice_id"
+  add_index "lines", ["invoice_id"], name: "index_lines_on_invoice_id", using: :btree
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -99,8 +102,8 @@ ActiveRecord::Schema.define(version: 20140623234822) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "years", force: true do |t|
     t.integer  "year"
