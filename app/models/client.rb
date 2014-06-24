9560e6edb7c6be2efda79f73adee6d0e1fe2d771
Robert Sefer <rsefer@gmail.com>
2014-06-23 23:33:05 -0500

    Added AWS S3 for image uploads

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,10 @@
 class Client < ActiveRecord::Base
 	has_many :projects, dependent: :destroy
-	has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/clients/:style/missing.png"
+	has_attached_file :logo,
+		:styles => { :medium => "300x300>", :thumb => "100x100>" },
+		:default_url => "/images/clients/:style/missing.png",
+		:storage => :s3,
+		:s3_credentials => "#{Rails.root}/config/aws.yml",
+    :path => '/images/:id/:style.:extension'
 	validates_attachment_content_type :logo, :content_type => %w(image/jpeg image/jpg image/png)
 end
