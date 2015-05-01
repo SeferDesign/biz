68ece3fe0c8a4ae80f0fc86de38f27e55bb1868f
Robert Sefer <rsefer@gmail.com>
2015-05-01 08:51:53 -0500

    Added svg support

--- app/models/client.rb
+++ app/models/client.rb
@@ -9,7 +9,7 @@ class Client < ActiveRecord::Base
 		:storage => :s3,
 		:s3_credentials => "#{Rails.root}/config/aws.yml",
     :path => '/images/:id/:style.:extension'
-	validates_attachment_content_type :logo, :content_type => %w(image/jpeg image/jpg image/png)
+	validates_attachment_content_type :logo, :content_type => %w(image/jpeg image/jpg image/png image/svg)
 
 	default_scope { order('name ASC') }
 
