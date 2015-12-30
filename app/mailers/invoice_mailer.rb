88433b0a8bc45400b8e9661acd1fe214429ac1f5
Robert Sefer <rsefer@gmail.com>
2015-12-30 15:14:51 -0600

    Removed old email method

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -9,45 +9,6 @@ class InvoiceMailer < ActionMailer::Base
     @mandrill_client ||= Mandrill::API.new Figaro.env.mandrill_api_key
   end
 
-  def invoice_email_old(invoice)
-    @invoice = invoice
-    @client = Client.find(@invoice.client_id)
-
-    @ourCCEmail = 'Robert Sefer <rsefer@gmail.com>'
-
-    if @client.email_accounting_2.present? and @client.email_accounting_3.present?
-      @emailCC = @client.email_accounting_2 + ', ' + @client.email_accounting_3 + ', ' + @ourCCEmail
-    elsif @client.email_accounting_2.present?
-      @emailCC = @client.email_accounting_2 + ', ' + @ourCCEmail
-    else
-      @emailCC = @ourCCEmail
-    end
-
-    @emailSubject = 'Invoice from Sefer Design Co.'
-    @pdfFileName = @client.name.gsub(/[^0-9A-Za-z]/, '') + @invoice.date.to_s
-
-    if @invoice.paid == true
-      @pdfFileName = @pdfFileName + '_paid'
-    end
-
-    attachments[@pdfFileName + '.pdf'] = WickedPdf.new.pdf_from_string(
-      render_to_string(
-        :pdf => @pdfFileName,
-        :layout => 'pdf/invoice.html',
-        :template => 'invoices/show.pdf.erb'
-      )
-    )
-
-    mail(
-      to: @client.email_accounting,
-      cc: @emailCC,
-      subject: @emailSubject
-    ) do |format|
-      format.html { render 'invoice_email.html' }
-      #format.text { render text: 'invoice_email.txt' }
-    end
-  end
-
   def invoice_email(invoice)
     @invoice = invoice
 
