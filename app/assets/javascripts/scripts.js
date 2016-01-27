72fd92c7e950e5621ee07b4cb16ae4a6ae621a78
Robert Sefer <rsefer@gmail.com>
2016-01-27 15:39:57 -0600

    Added "paid today" auto fill button

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,5 +1,21 @@
 jQuery(document).ready(function($) {
 
+	$('#paid-date-icon').click(function() {
+		var dateToday = new Date();
+		var dateYear = dateToday.getFullYear().toString();
+		var dateMonth = (dateToday.getMonth() + 1).toString();
+		if (dateMonth.length == 1) {
+			dateMonth = '0' + dateMonth;
+		}
+		var dateDay = dateToday.getDate().toString();
+		if (dateDay.length == 1) {
+			dateDay = '0' + dateDay;
+		}
+		var dateAsString = dateYear + '-' + dateMonth + '-' + dateDay;
+		$('#invoice_paiddate').val(dateAsString);
+		$('#invoice_paid_true').prop('checked', true);
+	});
+
 	$('.datepicker').datepicker({
 		format: 'yyyy-mm-dd'
 	});
