71466f3e5624053107e75e86527611f2f32e7eb7
Robert Sefer <rsefer@gmail.com>
2016-01-28 12:12:31 -0600

    DRYed invoices

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,18 +1,21 @@
 class LinesController < ApplicationController
+	before_action :set_invoice
 
 	def create
-    @invoice = Invoice.find(params[:invoice_id])
     @line = @invoice.lines.create(params[:line].permit(:description, :hourly, :hours, :rate, :total, :discount))
     @invoice.update(:cost => @invoice.lines.sum('total'))
     redirect_to invoice_path(@invoice)
   end
 
   def destroy
-    @invoice = Invoice.find(params[:invoice_id])
-    @line = @invoice.lines.find(params[:id])
-    @line.destroy
+    @invoice.lines.find(params[:id]).destroy
     @invoice.update(:cost => @invoice.lines.sum('total'))
     redirect_to invoice_path(@invoice)
   end
+
+	private
+		def set_invoice
+			@invoice = Invoice.find(params[:invoice_id])
+		end
 
 end
