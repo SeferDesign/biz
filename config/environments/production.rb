b7caee48c89310c423dbb6d48ecfd5c394cf053a
Robert Sefer <rsefer@gmail.com>
2016-10-24 12:20:36 -0500

    Added https to mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Biz::Application.configure do
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
-  config.action_mailer.default_url_options = { host: 'biz.seferdesign.com' }
+  config.action_mailer.default_url_options = { host: 'biz.seferdesign.com', protocol: 'https' }
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
