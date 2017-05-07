17a1595d93aa18a071a72d4ad399dfaaa3c8f7fe
Robert Sefer <rsefer@gmail.com>
2017-05-06 22:16:11 -0500

    Ordering

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,7 +2,7 @@ class Expense < ApplicationRecord
   belongs_to :vendor
   validates_presence_of :vendor_id, :name, :cost, :date
 
-  default_scope { order('date ASC') }
+  default_scope { order('date ASC, name ASC') }
 
   scope :recent, -> { where("date >= ? AND date <= ?", 1.year.ago, Time.now) }
   scope :future, -> { where(['date > ?', Time.now]) }
