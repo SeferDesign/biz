f3d6ecffb1451b01d4a40397e2d13393ea90609b
Robert Sefer <rsefer@gmail.com>
2017-05-06 19:16:44 -0500

    Added summary emails

--- app/mailers/summary_mailer.rb
+++ app/mailers/summary_mailer.rb
@@ -13,7 +13,28 @@ class SummaryMailer < ActionMailer::Base
     html_roadie = Roadie::Document.new email_html_raw
     html_inlined = html_roadie.transform
 
-    mail(to: "Robert Sefer <rsefer@gmail.com>", subject: "Weekly Invoice Summary - #{@date_start.strftime("%b %e")} through #{@date_end.strftime("%b %e")}") do |format|
+    mail(to: "Robert Sefer <rsefer@gmail.com>", subject: "Weekly Activity Summary - #{@date_start.strftime("%b %e")} through #{@date_end.strftime("%b %e")}") do |format|
+      format.html do
+        html_inlined
+      end
+    end
+
+  end
+
+  def monthly_activity_summary
+    last_date = Time.now.beginning_of_month - 1.day
+    @date_start = last_date.beginning_of_month
+    @date_end = last_date.end_of_month
+    @invoicesSent = Invoice.where(:date => @date_start..@date_end).order(date: :asc)
+    @invoicesPaid = Invoice.paid.where(:paiddate => @date_start..@date_end).order(paiddate: :asc)
+    @expenses = Expense.where(:date => @date_start..@date_end).order(date: :asc)
+
+    email_html_raw = render_to_string(:layout => 'layouts/mail.html', :action => 'monthly_activity_summary.html', :locals => { :invoicesSent => @invoicesSent, :invoicesPaid => @invoicesPaid, :expenses => @expenses, :date_start => @date_start, :date_end => @date_end })
+
+    html_roadie = Roadie::Document.new email_html_raw
+    html_inlined = html_roadie.transform
+
+    mail(to: "Robert Sefer <rsefer@gmail.com>", subject: "Monthly Activity Summary - #{@date_start.strftime("%b %e")} through #{@date_end.strftime("%b %e")}") do |format|
       format.html do
         html_inlined
       end
--- lib/tasks/summary.rake
+++ lib/tasks/summary.rake
@@ -1,4 +1,12 @@
-desc "Send weekly activity summary email"
-task :weekly_activity_summary => :environment do
-  SummaryMailer.weekly_activity_summary.deliver
+desc "Activity summaries"
+namespace :summary do
+  desc "Send weekly activity summary email"
+  task :weekly => :environment do
+    SummaryMailer.weekly_activity_summary.deliver
+  end
+
+  desc "Send monthly activity summary email"
+  task :monthly => :environment do
+    SummaryMailer.monthly_activity_summary.deliver
+  end
 end
