4e507018c7a34aa3d739a69d5df5b0a438066910
Robert Sefer <rsefer@gmail.com>
2017-12-27 22:19:12 -0600

    DB fix

--- db/schema.rb
+++ db/schema.rb
@@ -16,29 +16,29 @@ ActiveRecord::Schema.define(version: 20171204200418) do
   enable_extension "plpgsql"
 
   create_table "clients", force: :cascade do |t|
-    t.string   "name",                  limit: 255
-    t.string   "contact",               limit: 255
-    t.string   "site_url",              limit: 255
-    t.string   "logo",                  limit: 255
+    t.string   "name"
+    t.string   "contact"
+    t.string   "site_url"
+    t.string   "logo"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "logo_file_name",        limit: 255
-    t.string   "logo_content_type",     limit: 255
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
-    t.string   "address1",              limit: 255
-    t.string   "address2",              limit: 255
+    t.string   "address1"
+    t.string   "address2"
     t.integer  "zipcode"
-    t.string   "city",                  limit: 255
-    t.string   "state",                 limit: 255
+    t.string   "city"
+    t.string   "state"
     t.boolean  "international"
-    t.string   "intinfo",               limit: 255
-    t.string   "email_accounting",      limit: 255
-    t.string   "email_accounting_2",    limit: 255
-    t.string   "preferred_paymenttype", limit: 255
+    t.string   "intinfo"
+    t.string   "email_accounting"
+    t.string   "email_accounting_2"
+    t.string   "preferred_paymenttype"
     t.integer  "currentrate"
-    t.boolean  "active",                            default: true
-    t.string   "email_accounting_3",    limit: 255
+    t.boolean  "active",                default: true
+    t.string   "email_accounting_3"
     t.string   "federalein"
   end
 
@@ -57,9 +57,9 @@ ActiveRecord::Schema.define(version: 20171204200418) do
   create_table "goals", force: :cascade do |t|
     t.date     "startdate"
     t.date     "enddate"
-    t.string   "timeperiod", limit: 255
-    t.string   "goaltype",   limit: 255
-    t.decimal  "amount",                 precision: 8, scale: 2
+    t.string   "timeperiod"
+    t.string   "goaltype"
+    t.decimal  "amount",     precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -68,11 +68,11 @@ ActiveRecord::Schema.define(version: 20171204200418) do
     t.integer  "client_id"
     t.integer  "project_id"
     t.date     "date"
-    t.string   "worktype",     limit: 255
-    t.decimal  "cost",                     precision: 8, scale: 2
+    t.string   "worktype"
+    t.decimal  "cost",         precision: 8, scale: 2
     t.boolean  "paid"
     t.date     "paiddate"
-    t.string   "paymenttype",  limit: 255
+    t.string   "paymenttype"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -81,20 +81,20 @@ ActiveRecord::Schema.define(version: 20171204200418) do
   end
 
   create_table "lines", force: :cascade do |t|
-    t.string   "description", limit: 255
+    t.string   "description"
     t.boolean  "hourly"
     t.float    "hours"
-    t.decimal  "rate",                    precision: 8, scale: 2
-    t.decimal  "total",                   precision: 8, scale: 2
+    t.decimal  "rate",        precision: 8, scale: 2
+    t.decimal  "total",       precision: 8, scale: 2
     t.integer  "invoice_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "discount",                                        default: false
+    t.boolean  "discount",                            default: false
     t.index ["invoice_id"], name: "index_lines_on_invoice_id", using: :btree
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.integer  "client_id"
     t.date     "startdate"
     t.date     "enddate"
@@ -104,16 +104,16 @@ ActiveRecord::Schema.define(version: 20171204200418) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
