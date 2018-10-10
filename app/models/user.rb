c582174b7bb2d8ce4e7b8e5f8255da167428e0a1
Robert Sefer <rsefer@gmail.com>
2018-10-10 12:01:54 -0500

    Bump

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :registerable, :recoverable, :rememberable, :trackable, :validatable
 end
