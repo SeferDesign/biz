bc76a82fa243002c17cec997564d9c5fe4a6106f
Robert Sefer <rsefer@gmail.com>
2018-10-19 15:33:58 -0500

    Stripe dash

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,12 @@ class WelcomeController < ApplicationController
   def payment
   end
 
+	def stripe
+		Stripe.api_key = Figaro.env.stripe_api_secret_key
+		@payouts = Stripe::Payout.list(limit: 9999)
+		@charges = Stripe::Charge.list(limit: 9999)
+	end
+
   def drive
     require "google_drive"
     session = GoogleDrive::Session.from_config("config/google.json")
