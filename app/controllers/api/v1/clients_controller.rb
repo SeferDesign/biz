274fa8c0e6e2ce38e2557c38c82d1513209982a1
Robert Sefer <rsefer@gmail.com>
2019-11-02 10:38:31 -0500

    Custom sort method

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -2,7 +2,10 @@ class Api::V1::ClientsController < Api::ApiController
   before_action :set_client, only: [:show]
 
   def index
-    @clients = Client.all
+		@clients = Client.all
+		if params[:sortBy] == 'date'
+			@clients = @clients.sort { |a,b| a.mostRecentInvoiceDate <=> b.mostRecentInvoiceDate }
+		end
     respond_with @clients, methods: :mostRecentInvoiceDate
   end
 
