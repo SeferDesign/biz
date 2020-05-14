50ca5c8f4a9c70fb50bdd439666b8bbcde7dc95a
Robert Sefer <rsefer@gmail.com>
2020-05-14 17:28:29 -0500

    Woops

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -33,7 +33,7 @@ class InvoicesController < ApplicationController
 			credentials.fetch_access_token!
 			@session = GoogleDrive::Session.from_credentials(credentials)
 
-			puts refresh_token.inspect
+			puts credentials.inspect
 			User.find(current_user.id).update({ google_token: credentials.refresh_token })
 		end
 
