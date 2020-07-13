3f74759a1f6bd56da428f69f9d5db33c3f26c143
Robert Sefer <rsefer@gmail.com>
2020-07-13 15:18:37 -0500

    Simpler plaintext invoice email

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -13,16 +13,6 @@ class InvoiceMailer < ActionMailer::Base
       cc_list.push(invoice.client.email_accounting_3)
     end
 
-    email_html_raw = render_to_string(:layout => 'layouts/mail.html', :action => 'invoice_email.html', :locals => { :invoice => @invoice })
-    html_roadie = Roadie::Document.new email_html_raw
-		html_inlined = html_roadie.transform
-
-		emailSubject = 'Invoice from Sefer Design Company'
-
-		if Rails.env.development?
-			emailSubject = 'DEV - ' + emailSubject
-		end
-
 		attachments["#{invoice.pdfFileName}.pdf"] = WickedPdf.new.pdf_from_string(
 			render_to_string(
 				:layout => 'pdf/invoice.html',
@@ -36,11 +26,11 @@ class InvoiceMailer < ActionMailer::Base
 			:page_size => 'Letter'
 		)
 
-    mail(to: "#{invoice.client.contact} <#{invoice.client.email_accounting}>", subject: emailSubject, cc: cc_list) do |format|
-      format.html do
-        html_inlined
-      end
-    end
+    mail(to: "#{invoice.client.contact} <#{invoice.client.email_accounting}>", subject: (Rails.env.development? ? 'DEV - ' : '') + 'Invoice from Sefer Design Company', cc: cc_list, body: render_to_string(
+			:layout => 'layouts/mail.txt',
+			:action => 'invoice_email.txt',
+			:locals => { :invoice => @invoice }
+		))
 
   end
 
