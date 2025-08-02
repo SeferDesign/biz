e0f0eb47eae3e921e13ce6a75cfec34fef9ef32b
Robert Sefer <rsefer@gmail.com>
2025-08-02 16:26:56 -0500

    Not not

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::ClientsController < Api::ApiController
 			@clients = @clients.sort { |a,b| a.mostRecentActivityDate <=> b.mostRecentActivityDate }.reverse
 		end
 		if params[:filter] == 'active'
-			@clients.select! { |c| not c.archived == false }
+			@clients.select! { |c| not c.archived }
 		elsif params[:filter] == 'archived'
 			@clients.select! { |c| c.archived }
 		end
